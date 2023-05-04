6ec5fc067e4e8843e72400faa5bce30ca48382c2
root <651360107@qq.com>
2023-05-04 10:37:16 +0000

    packet

---
+++ bpf/packet/packet.c
@@ -0,0 +1,17 @@
+#include "packet.h"
+BPF_HASH(packets);
+int hello_packet(struct xdp_md *ctx) {
+    u64 counter = 0;
+    u64 key = 0;
+    u64 *p;
+    key = lookup_protocol(ctx);
+    if (key != 0) {
+        p = packets.lookup(&key);
+        if (p != 0) {
+            counter = *p;
+        }
+        counter++;
+        packets.update(&key, &counter);
+    }
+    return XDP_PASS;
+}
