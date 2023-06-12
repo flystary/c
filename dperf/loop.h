b57958de20b8a4c33c267273cf055edfcd9ebde5
aix <651360107@qq.com>
2023-06-12 22:15:58 +0800

    loop

--- dperf/loop.h
+++ dperf/loop.h
@@ -16,5 +16,25 @@ typedef void(*l3_input_t)(struct work_space *, struct rte_mbuf *, l4_input_t, l4
 typedef int(*work_space_process_t)(struct work_space *);
 
 
+static inline int vxlan_check(struct work_space *ws, struct vxlan_headers*vxhs)
+{
+    struct udphdr *uh = NULL;
+    struct iphdr *iph = NULL;
 
+    iph = &vxhs->iph;
+    uh  = &vxhs->uh;
+    
+    if ((iph->protocol == IPPROTO_DUP) &&
+        (iph->daddr == ws->vtep_ip) &&
+        (uh->dest == htons(VXLAN_PORT)) &&
+        (vxhs->vxh.vni == ws->vni)) {
+        return 0;
+    }
+    return -1;
+}
+
+static inline void vxlan_input(struct work_space *ws, struct rte_mbuf *m, 
+    l4_input_t tcp_input, l4_input_t udp_input)
+{
+}
 #endif
